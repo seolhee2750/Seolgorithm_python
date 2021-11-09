a183a8b49e054da296b57618662ecce7677c85a2
김설희 <2750seolhee@naver.com>
2021-11-09 17:52:00 +0900

    k번째 수 문제 풀이

---
+++ Programmers/Level1/k번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    for c in commands:
+        i = c[0]; j = c[1]; k = c[2]
+        tmp = sorted(array[i-1:j])
+        answer.append(tmp[k-1])
+    return answer
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
