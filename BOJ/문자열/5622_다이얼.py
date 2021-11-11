9c77e35416e1e653e45c881f4e88ae3064f92095
김설희 <2750seolhee@naver.com>
2021-11-12 00:07:36 +0900

    5622 다이얼 문제 풀이

---
+++ BOJ/문자열/5622_다이얼.py
@@ -0,0 +1,7 @@
+s = input()
+alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+dial = [3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10]
+answer = 0
+for i in s:
+    answer += dial[alphabet.index(i)]
+print(answer)
