6bd81afb759b7ec8dcbde443bba33a13c46b1ab7
김설희 <2750seolhee@naver.com>
2022-02-03 19:50:28 +0900

    모의고사 두 번째 풀이

--- Programmers/Level1/모의고사.py
+++ Programmers/Level1/모의고사.py
@@ -16,3 +16,27 @@ def solution(answers):
 
 print(solution([1,2,3,4,5]))
 print(solution([1,3,2,4,2]))
+
+"""
+[ 두 번째 풀이 ] 
+
+def solution(answers):
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    count = [0, 0, 0]
+    result = []
+    
+    for i in range(len(answers)):
+        if answers[i] == first[i % 5]: count[0] += 1
+        if answers[i] == second[i % 8]: count[1] += 1
+        if answers[i] == third[i % 10]: count[2] += 1
+
+    maxCount = max(count)
+    for i in range(3):
+        if count[i] == maxCount: result.append(i+1)
+
+    result.sort()
+    return result
+"""
+
