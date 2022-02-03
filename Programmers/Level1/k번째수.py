626133403a0cb609db9857977dbf7e2e734931a4
김설희 <2750seolhee@naver.com>
2022-02-03 21:08:52 +0900

    k번째 수 문제 풀이

--- Programmers/Level1/k번째수.py
+++ Programmers/Level1/k번째수.py
@@ -7,3 +7,16 @@ def solution(array, commands):
     return answer
 
 print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+
+"""
+[ 두 번째 풀이 ]
+
+def solution(array, commands):
+    answer = []
+    
+    for c in commands:
+        i, j, k = c[0], c[1], c[2]
+        answer.append(sorted(array[i-1:j])[k-1])
+
+    return answer
+"""
