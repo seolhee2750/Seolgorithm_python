ea731c01241585a606d383047b3f3d2e7069a4c0
김설희 <2750seolhee@naver.com>
2022-02-04 21:01:05 +0900

    땅따먹기 문제 풀이

---
+++ Programmers/Level2/땅따먹기.py
@@ -0,0 +1,14 @@
+def solution(land):
+    if len(land) == 1:
+        return max(land[0])
+    else:
+        for i in range(1, len(land)):
+            land[i][0] += max(land[i - 1][1], land[i - 1][2], land[i - 1][3])
+            land[i][1] += max(land[i - 1][0], land[i - 1][2], land[i - 1][3])
+            land[i][2] += max(land[i - 1][0], land[i - 1][1], land[i - 1][3])
+            land[i][3] += max(land[i - 1][0], land[i - 1][1], land[i - 1][2])
+
+        return max(land[-1])
+
+print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
+print(solution([[0,0,0,0]]))
