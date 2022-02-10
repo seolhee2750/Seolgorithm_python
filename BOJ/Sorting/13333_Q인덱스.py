a0627597a1fcf6744e2a9ded9c2f38171aa64e14
김설희 <2750seolhee@naver.com>
2022-02-10 23:42:37 +0900

    13333 Q-인덱스 문제 풀이

--- BOJ/Sorting/13333_Q인덱스.py
+++ BOJ/Sorting/13333_Q인덱스.py
@@ -1,1 +1,15 @@
 # BOJ #13333 Q-인덱스
+
+n = int(input())
+nums = list(map(int, input().split()))
+nums.sort()
+m = nums[-1]
+q = 0
+
+for i in reversed(range(1, m)):
+    tmp = list(filter(lambda x: x >= i, nums))
+    if len(tmp) >= i:
+        q = i
+        break
+
+print(q)
