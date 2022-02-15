4f502ccde059238fcac71d981b199bdb56a2ff4c
김설희 <2750seolhee@naver.com>
2022-02-15 15:13:49 +0900

    15649 문제 풀이

--- BOJ/BruteForce/15649_N과M1.py
+++ BOJ/BruteForce/15649_N과M1.py
@@ -1,1 +1,16 @@
 # BOJ #15649 N과 M (1) (백트래킹 입문예제)
+
+n, m = map(int, input().split())
+tmp = []
+
+def dfs():
+    if len(tmp) == m:
+        print(' '.join(map(str, tmp)))
+        return
+    for i in range(1, n+1):
+        if i not in tmp:
+            tmp.append(i)
+            dfs()
+            tmp.pop()
+
+dfs()
