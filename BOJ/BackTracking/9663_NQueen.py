514e2fc4f47ef5e02684809743d67c2bd7b3519b
김설희 <2750seolhee@naver.com>
2022-02-15 17:05:34 +0900

    N-Queen 문제 풀이 (시간 초과)

--- BOJ/BackTracking/9663_NQueen.py
+++ BOJ/BackTracking/9663_NQueen.py
@@ -1,1 +1,27 @@
 # BOJ 9663 N-Queen (백트래킹 대표예제)
+
+n = int(input())
+count = 0
+row = [0]*n
+check = True
+
+def dfs(x):
+    global count
+    global check
+
+    if x == n:
+        count += 1
+    else:
+        for i in range(n):
+            row[x] = i
+            for j in range(x):
+                if row[j] == row[x] or abs(row[j] - row[x]) == abs(j - x):
+                    check = False
+                    break
+            if check == True:
+                dfs(x + 1)
+            else:
+                check = True
+
+dfs(0)
+print(count)
