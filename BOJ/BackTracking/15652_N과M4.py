f81f49ae258629dc0891f52c25c3e5e5e2f9018c
김설희 <2750seolhee@naver.com>
2022-02-15 15:45:58 +0900

    N과 M (2) 풀이 수정

--- BOJ/BackTracking/15652_N과M4.py
+++ BOJ/BackTracking/15652_N과M4.py
@@ -1,4 +1,4 @@
-# BOJ #15650 N과 M (2)
+# BOJ #15652 N과 M (4)
 
 n, m = map(int, input().split())
 tmp = []
@@ -8,7 +8,7 @@ def dfs():
         print(' '.join(map(str, tmp)))
         return
     for i in range(1, n+1):
-        if len(tmp) == 0 or tmp[-1] < i:
+        if len(tmp) == 0 or tmp[-1] <= i:
             tmp.append(i)
             dfs()
             tmp.pop()
