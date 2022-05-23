23e7b743a81f4906c963d9fa924780f101d16b4b
김설희 <2750seolhee@naver.com>
2022-05-23 18:11:47 +0900

    1316 그룹단어체커 두 번째 풀이

--- BOJ/문자열/1316_그룹단어체커.py
+++ BOJ/문자열/1316_그룹단어체커.py
@@ -18,3 +18,29 @@ for _ in range(0, n):
     if answer == True: count += 1
 
 print(count)
+
+"""
+두 번째 풀이
+
+n = int(input())
+result = 0
+
+for i in range(n):
+    s = input()
+    check = []
+    go = True
+
+    for i in s:
+        if i in check:
+            if check.index(i) != len(check)-1:
+                go = False
+        else:
+            check.append(i)
+        if not go:
+            break
+
+    if go:
+        result += 1
+
+print(result)
+"""
