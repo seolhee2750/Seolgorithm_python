2c073cf3e72bc25c52aca419026049209705c171
김설희 <2750seolhee@naver.com>
2022-05-29 00:25:37 +0900

    11866 요세푸스 문제 0 두 번째 문제 풀이

--- BOJ/Queue-Deque/11866_요세푸스문제0.py
+++ BOJ/Queue-Deque/11866_요세푸스문제0.py
@@ -3,6 +3,23 @@ from collections import deque
 
 n, k = map(int, sys.stdin.readline().strip().split())
 queue = deque(i+1 for i in range(n))
+answer = []
+
+while queue:
+    for i in range(k-1):
+        queue.append(queue.popleft())
+    answer.append(str(queue.popleft()))
+
+print("<" + ", ".join(answer) + ">")
+
+"""
+첫 번째 풀이
+
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().strip().split())
+queue = deque(i+1 for i in range(n))
 tmp = deque()
 cnt = 0
 idx = 1
@@ -24,3 +41,4 @@ while cnt < n:
         continue
 
 print("<" + ", ".join(answer) + ">")
+"""
