3801b4c6ff9175467d9ed9ff00257a1b26d28731
김설희 <2750seolhee@naver.com>
2022-06-16 11:17:55 +0900

    1927 최소 힙 문제 풀이

---
+++ BOJ/Queue-Deque/1927_최소힙.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline().strip())
+heap = []
+for _ in range(n):
+    now = int(sys.stdin.readline().strip())
+    if now == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, now)
